cd73219e433afe9c3f8ac6d4620b1f024b31dba9
scottzwells <2228630475@qq.com>
2025-06-16 11:04:00 +0800

    添加盘子柜台
    每3s自动生成一个盘子，最多5个

---
+++ Assets/Scripts/Counter/PlatesCounter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    [SerializeField] private float spawnRate = 3;
+    [SerializeField] private KitchenObjectSO plateSO;
+    [SerializeField] private int plateCountMax = 5;
+
+    private float timer = 0;
+    private List<KitchenObject> platesList = new List<KitchenObject>();
+
+    private void Update()
+    {
+        if(platesList.Count < plateCountMax) timer += Time.deltaTime;
+
+        if (timer >= spawnRate && platesList.Count < plateCountMax)
+        {
+            CreatePlate();
+            timer = 0;
+        }
+    }
+
+    public void CreatePlate()
+    {
+        KitchenObject kitchenObject = GameObject.Instantiate(plateSO.prefab, GetHoldPoint()).GetComponent<KitchenObject>();
+
+        kitchenObject.transform.localPosition = Vector3.zero + Vector3.up * 0.2f * platesList.Count;
+        platesList.Add(kitchenObject);
+    }
+
+    public override void Interact(Player player)
+    {
+        if (player.IsHaveKitchenObject() == false)
+        {// 手上无食材
+            if(platesList.Count > 0)
+            {
+                player.AddKitchenObject(platesList[platesList.Count -1]);   // 玩家取走最后一个盘子
+                platesList.RemoveAt(platesList.Count - 1);
+            }
+        }
+    }
+}
